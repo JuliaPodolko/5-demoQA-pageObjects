deaeb1aa701ea9d20d18c32e06b5d74f902d5bab
Julia Podolko <Julia Podolkoemail>
2022-05-15 01:29:04 +0500

    added annotations

--- src/test/java/simple.java
+++ src/test/java/simple.java
@@ -1,2 +1,21 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
 public class simple {
+    @BeforeEach
+    void openYaPage() {
+        Selenide.open ("https://ya.ru");
+    }
+    @AfterAll
+    void close () {
+        WebDriverRunner.closeWindow();
+    }
+    @Test
+    void assertTest() {
+
+//.......
+
+
+    }
 }
